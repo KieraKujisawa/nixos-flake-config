3e35c37aace4be50f9199c189a3088ddf3314844
Kiera Meredith <kiera@meredithkm.info>
2025-05-25 19:00:47 -0400

    Enable experimental features [ "nix-command" "flakes" ]

--- system/configuration.nix
+++ system/configuration.nix
@@ -17,6 +17,9 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
