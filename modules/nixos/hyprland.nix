572931787f1bf5bde8a10b59b4d41e6deae3815f
Kiera Meredith <kiera@meredithkm.info>
2025-05-26 23:48:11 -0400

    tweak hyprland setup

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,22 +1,26 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
-    programs.hyprland = {
-        enable = true;
-    };
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
 
-    environment.systemPackages = with pkgs; [
-      hyprpaper
-      kitty
-      libnotify
-      mako
-      qt5.qtwayland
-      qt6.qtwayland
-      swayidle
-      swaylock-effects
-      wlogout
-      wl-clipboard
-      wofi
-      waybar
-    ];
+  environment.systemPackages = with pkgs; [
+    hyprpaper
+    kitty
+    libnotify
+    mako
+    qt5.qtwayland
+    qt6.qtwayland
+    swayidle
+    swaylock-effects
+    wlogout
+    wl-clipboard
+    wofi
+    waybar
+  ];
 }
