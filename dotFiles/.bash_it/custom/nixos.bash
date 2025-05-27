b60b428c904181497150b3f3b9190c48e7a721c6
Kiera Meredith <kiera@meredithkm.info>
2025-05-26 20:07:14 -0400

    update nixos aliases

--- dotFiles/.bash_it/custom/nixos.bash
+++ dotFiles/.bash_it/custom/nixos.bash
@@ -1,3 +1,3 @@
-alias nrs='sudo nixox-rebuild switch --flake $HOME/src/KieraKujisawa/nixos-flake-config'
-alias hms='home-manager switch --flake $HOME/src/KieraKujisawa/nixos-flake-config#kieram@nixos'
+alias nrs='sudo nixos-rebuild switch --flake $HOME/src/KieraKujisawa/nixos-flake-config'
+alias hms='home-manager switch --flake $HOME/src/KieraKujisawa/nixos-flake-config#kieram@nixos -b backup'
 alias nixw='cd $HOME/src/KieraKujisawa/nixos-flake-config'
