41fdc2ac647bcb071baed34f1e5f45230738f44e
Kiera Meredith <kiera@meredithkm.info>
2025-05-26 23:49:06 -0400

    Update dot files

--- dotFiles/.bash_it/custom/nixos.bash
+++ dotFiles/.bash_it/custom/nixos.bash
@@ -1,3 +1,4 @@
+alias nfu='cd $HOME/src/KieraKujisawa/nixos-flake-config; sudo nix flake update'
 alias nrs='sudo nixos-rebuild switch --flake $HOME/src/KieraKujisawa/nixos-flake-config'
 alias hms='home-manager switch --flake $HOME/src/KieraKujisawa/nixos-flake-config#kieram@nixos -b backup'
 alias nixw='cd $HOME/src/KieraKujisawa/nixos-flake-config'
